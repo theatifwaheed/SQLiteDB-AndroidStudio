2fa67f6a74da2764a01f381f17f1d936fb9866be
TheAtifWaheed <theatifwaheed@gmail.com>
2021-12-09 14:10:21 +0500

    SQLHelper.

--- app/src/main/java/com/theatifwaheed/labdatabasesql/SQLiteDB_Helper.java
+++ app/src/main/java/com/theatifwaheed/labdatabasesql/SQLiteDB_Helper.java
@@ -1,6 +1,8 @@
 package com.theatifwaheed.labdatabasesql;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.widget.Toast;
@@ -23,11 +25,36 @@ public class SQLiteDB_Helper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(query_create);
+        Toast.makeText(mainContext, "Table Created.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public boolean insertStudentData (String s_id, String s_name, String s_city, String s_age){
+        SQLiteDatabase sql_db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("std_id", s_id);
+        contentValues.put("std_name", s_name);
+        contentValues.put("std_city", s_city);
+        contentValues.put("std_age", s_age);
+
+        long insertCheck = sql_db.insert(TABLE_NAME, null, contentValues);
+        if (insertCheck == -1){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public Cursor getStudentData(String s_id){
+        SQLiteDatabase sql_db = this.getWritableDatabase();
+        Cursor cursor = sql_db.rawQuery("select * from " + TABLE_NAME + " where std_id = ? ", new String[]{s_id});
+        return cursor;
     }
 }
